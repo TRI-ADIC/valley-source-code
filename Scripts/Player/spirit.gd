a769c148fb92a166adacc029aee896e62e861244
Marco <marco.anto.diaz@protonmail.com>
2026-05-21 15:11:10 -0400

    docs: change bug comment to better reflect the fact I don't know why it happens

--- Scripts/Player/spirit.gd
+++ Scripts/Player/spirit.gd
@@ -27,7 +27,7 @@ func _process(delta: float) -> void:
 	
 	_spirit_move_delay(delta)
 	
-	# Bug: Sending and retrieving spirit in quick succession causes spirit to come from player's origin point.
+	# Bug: Sending and retrieving spirit in quick succession causes spirit to come from... somewhere in the ground idk.
 	
 	# Sending out spirit
 	if Input.is_action_pressed("send"):
