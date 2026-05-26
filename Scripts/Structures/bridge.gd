8941e7bd19cf9065ff9fbe8d45ea98ca7398299a
Marco <marco.anto.diaz@protonmail.com>
2026-05-25 21:52:07 -0400

    feat: replace placeholder door with bridge and modify existing levels with it

--- Scripts/Structures/bridge.gd
+++ Scripts/Structures/bridge.gd
@@ -2,18 +2,12 @@ extends Node3D
 
 @export var open_with_flames:Dictionary[Cauldron, bool] = {}
 
-var open = false
+var lift = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for flame in open_with_flames:
 		flame.lit.connect(_check_if_lit)
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if open:
-		%DoorMesh.visible = false
-		%DoorMesh.use_collision = false
 
 # Check if all cauldrons are lit
 func _check_if_lit():
@@ -22,7 +16,11 @@ func _check_if_lit():
 		
 		# If all are lit, entrance opens
 		if is_lit:
-			open = true
+			lift = true
 		else:
-			open = false
+			lift = false
 			break
+			
+	if lift:
+		var tween = create_tween()
+		tween.tween_property(%BridgeMesh, "position", %RaisedPoint.position, 3).set_trans(Tween.TRANS_SINE)
