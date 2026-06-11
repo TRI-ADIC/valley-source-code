0a89df4dff4e6e8dfda8945a8158832479275b3b
Marco <marco.anto.diaz@protonmail.com>
2026-06-11 01:15:17 -0400

    feat: added scream to level 3

--- Scripts/World/world.gd
+++ Scripts/World/world.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var scream_triggered: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +19,12 @@ func _on_level_2_trigger_area_entered(area: Area3D) -> void:
 		GlobalAudio.play_valley_ambience()
 		var tween = create_tween()
 		tween.tween_property(GlobalAudio, "volume_db", 0, 10).set_trans(Tween.TRANS_LINEAR)
+
+
+func _on_level_3_trigger_area_entered(area: Area3D) -> void:
+	if !scream_triggered:
+		scream_triggered = true
+		GlobalAudio.stop()
+		%Scream.play()
+		var tween = create_tween()
+		tween.tween_property(%Scream, "volume_db", 20.935, 1).set_trans(Tween.TRANS_SINE)
